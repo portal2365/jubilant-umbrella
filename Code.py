ed1bfd5788119dbe52a14d7e4cd7d5612555f28a
portal2365 <43701661+portal2365@users.noreply.github.com>
2020-10-23 13:08:41 +0100

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,12 @@
+import ploty.figure_factory as ff
+
+import pandas as pd
+
+import csv
+
+
+df = pd.read_csv("data.csv")
+
+fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Avg Rating"])
+
+fig.show()
